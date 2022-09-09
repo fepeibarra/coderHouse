6241754af794c73df85831b5f34dfcbaa11647ca
fepeibarra <99020965+fepeibarra@users.noreply.github.com>
2022-09-08 21:36:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let altura = prompt ("Cual es tu altura?"); {
+
+    if (altura > 0 && altura <= 150) {
+            alert ("Sos petiso");
+        } else if (altura > 150 && altura <= 160) {
+            alert ("Sos bajito");
+        } else if  (altura > 160 && altura <=180) {
+            alert ("Sos mediano");
+        } else {
+            alert ("Sos alto");
+        }
+    }
